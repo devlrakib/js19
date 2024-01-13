f9436f95e675f34a33fb9c53d503642c180c5592
devlrakib <devrakib5@gmail.com>
2024-01-14 01:16:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// While Loop Er Khetre For Loop Theke 3 Ta Part E Alada
+// While Loop A while Loop Start Korar Age Starting Point Ba Initialization Part Ta Thakbe
+// While Loop Ar Pore Functional Section Er Moddhe Conditional Part Ta Thakbe
+// While Loop a Second Bracket Er Moddhe Increment Decrement Command Thakbe
+
+// First Example
+var i=1;
+while(i<=100){
+    document.write(" " +i+"</br>");
+    i=i+1;
+}
+
+// Second Example
+var i=1;
+var sum =0;
+while(i<=10){
+sum =sum+i;
+i=i+1;
+}
+document.write(sum);
+
+
